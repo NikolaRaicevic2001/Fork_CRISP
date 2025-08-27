115ad9d4778cb9a2dc22ff2c1ab86b24c2251db9
NikolaRaicevic2001 <nikolar047@gmail.com>
2025-08-27 15:20:42 -0700

    minor

--- src/examples/pushbox/VisualizePushBox.py
+++ src/examples/pushbox/VisualizePushBox.py
@@ -7,12 +7,12 @@ from matplotlib.animation import FuncAnimation
 
 # ---------------- PARAMETERS ----------------
 # Define model parameters for pushbox
-a = 0.1                        # half width of the box
-b = 0.1                        # half height of the box
+a = 0.5                        # half width of the box
+b = 0.7                        # half height of the box
 dt = 0.1                       # time step (100 ms)
 N  = 100                       # number of time steps
 num_state   = 3                # STATE  (3) : [px, py, θ]
-example_name = "pushbox_sdf"    
+example_name = "pushbox"    
 
 if example_name == "pushbox":
     method = "actual"
--- src/examples/pushbox/cpp/SolvePushbox.cpp
+++ src/examples/pushbox/cpp/SolvePushbox.cpp
@@ -8,8 +8,8 @@
 using namespace CRISP;
 
 // Define model parameters for pushbox
-const scalar_t a = 0.1;
-const scalar_t b = 0.1;
+const scalar_t a = 0.5;
+const scalar_t b = 0.7;
 const scalar_t m = 1;
 const scalar_t mu = 0.5;
 const scalar_t g = 9.8;
